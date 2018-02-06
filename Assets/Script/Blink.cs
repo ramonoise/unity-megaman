466e42a7515ba8be7705fe7e86f82832d7b74cc9
Ramon Honorio <ramon.honorio@miraeducacao.com.br>
2018-02-05 22:45:59 -0200

    initial commit

---
+++ Assets/Script/Blink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Blink : MonoBehaviour {
+
+	// Interval between one blink and another
+	public float interval;
+
+	IEnumerator Start () {
+
+		GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (interval);
+		GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (interval);
+
+		// Loops function
+		StartCoroutine(Start());
+	}
+
+	void Update() {
+
+		// Press enter
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			SceneManager.LoadScene ("game-scene");
+		}
+
+
+	}
+
+}
